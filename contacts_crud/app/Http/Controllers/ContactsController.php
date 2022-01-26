7b26244024949e62ca211ea95cc1b91f4861bb16
amorellb <amorellb@cifpfbmoll.eu>
2022-01-26 23:17:27 +0100

    feat: add contacts controller functions

--- contacts_crud/app/Http/Controllers/ContactsController.php
+++ contacts_crud/app/Http/Controllers/ContactsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contacts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ContactsController extends Controller
 {
@@ -14,7 +15,8 @@ class ContactsController extends Controller
      */
     public function index()
     {
-        //
+        $contacts = Contacts::all();
+        return view('contacts.index', compact('contacts'));
     }
 
     /**
@@ -24,62 +26,78 @@ class ContactsController extends Controller
      */
     public function create()
     {
-        //
+        return view('contacts.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request['slug'] = Str::slug($request->name, '-');
+
+        $contact = new Contacts();
+        $contact->name = $request->name;
+        $contact->slug = $request->slug;
+        $contact->birth_date = $request->birth_date;
+        $contact->email = $request->email;
+        $contact->phone = $request->phone;
+        $contact->country = $request->country;
+        $contact->address = $request->address;
+        $contact->job_contact = $request->job_contact;
+        $contact->user_id = $request->user()->id;
+        $contact->save();
+
+        return redirect()->route('contacts.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Contacts  $contacts
+     * @param \App\Models\Contacts $contacts
      * @return \Illuminate\Http\Response
      */
-    public function show(Contacts $contacts)
+    public function show(Contacts $contact)
     {
-        //
+        return view('contacts.show', compact('contact'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Contacts  $contacts
+     * @param \App\Models\Contacts $contacts
      * @return \Illuminate\Http\Response
      */
-    public function edit(Contacts $contacts)
+    public function edit(Contacts $contact)
     {
-        //
+        return view('contacts.edit', compact('contact'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Contacts  $contacts
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Contacts $contacts
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Contacts $contacts)
+    public function update(Request $request, Contacts $contact)
     {
-        //
+        $contact->update($request->all());
+        return redirect()->route('contacts.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Contacts  $contacts
+     * @param \App\Models\Contacts $contacts
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Contacts $contacts)
+    public function destroy(Contacts $contact)
     {
-        //
+        $contact->delete();
+        return redirect()->route('contacts.index');
     }
 }
