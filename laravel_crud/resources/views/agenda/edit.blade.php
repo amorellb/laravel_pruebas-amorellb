a716f91758c439c7b857780a9d7e3ad31990d7a7
amorellb <amorellb@cifpfbmoll.eu>
2022-01-26 16:51:54 +0100

    first commit for contacts_crud project

--- laravel_crud/resources/views/agenda/edit.blade.php
+++ laravel_crud/resources/views/agenda/edit.blade.php
@@ -8,7 +8,8 @@
         @method('PUT')
 
         <label for="name"> Contact name:
-            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="name" value="{{old('name', $contact->name)}}" placeholder="Bernat Smith"/>
+            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="name"
+                   value="{{old('name', $contact->name)}}" placeholder="Bernat Smith"/>
         </label>
         @error('name')
         <br>
@@ -17,10 +18,12 @@
         @enderror
         <br>
         <label for="email"> Contact email:
-            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="email" value="{{old('email', $contact->email)}}" placeholder="bernat@email.com"/>
+            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="email"
+                   value="{{old('email', $contact->email)}}" placeholder="bernat@email.com"/>
         </label><br>
         <label for="phone"> Contact phone:
-            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="phone" value="{{old('phone', $contact->phone)}}" placeholder="654321234"/>
+            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="phone"
+                   value="{{old('phone', $contact->phone)}}" placeholder="654321234"/>
         </label>
         @error('phone')
         <br>
@@ -29,8 +32,12 @@
         @enderror
         <br>
         <label for="address"> Contact address:
-            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="address" value="{{old('address', $contact->address)}}" placeholder="Address 123, street"/>
+            <input class="border-2 border-solid border-gray-100 rounded-full px-2" type="text" name="address"
+                   value="{{old('address', $contact->address)}}" placeholder="Address 123, street"/>
         </label><br>
-        <button class="text-orange-400 no-underline border-solid border-2 border-orange-400 rounded p-1 px-5 ml-5 mt-5 hover:bg-orange-400 hover:text-white" type="submit" name="add">📝 Edit</button>
+        <button
+            class="text-orange-400 no-underline border-solid border-2 border-orange-400 rounded p-1 px-5 ml-5 mt-5 hover:bg-orange-400 hover:text-white"
+            type="submit" name="add">📝 Edit
+        </button>
     </form>
 </main>
